ce1ff447ef770433e0699aa8ba953335b7ed5b08
Juan Benegas <jsbenegasr@gmail.com>
2024-07-25 12:42:50 -0400

    Third Commit

--- A/A/settings.py
+++ A/A/settings.py
@@ -31,12 +31,16 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'parcels.apps.ParcelsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'import_export',
+    'rest_framework',
+
 ]
 
 MIDDLEWARE = [
@@ -105,7 +109,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Asuncion'
 
 USE_I18N = True
 
--- A/parcels/models.py
+++ A/parcels/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from import_export import resources
+
 
 # Create your models here.
+
+class lote(models.Model):
+    codigo = models.CharField(max_length=11, primary_key=True)
+    manzana = models.IntegerField(default=0, null=True, blank=True)
+    lote = models.IntegerField(default=0, null=True, blank=True)
+    estado = models.CharField(max_length=10)
+    fecha = models.DateField("fecha", null=True, blank=True)
+    vendedor = models.CharField(max_length=20, null=True, blank=True)
+
+    def __str__(self):
+        return f"Lote {self.lote} in Manzana {self.manzana} (Código: {self.codigo}), " f"Estado: {self.estado},  "f"Vendedor: {self.vendedor}"
+
+    def formatted_date(self):
+        if self.date_field:
+            return self.date_field.strftime('%m-%d-%Y')
+        return 'N/A'  # or any other placeholder for None values
+
+
+class loteResource(resources.ModelResource):
+    class Meta:
+        model = lote
+        import_fields = ["codigo"]
+        skip_unchanged = True
+        use_bulk = True
